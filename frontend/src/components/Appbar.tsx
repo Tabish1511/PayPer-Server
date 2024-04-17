e35131aed35f7d1895278af2f0993b312221c401
Tabish1511 <khaqantabish@gmail.com>
2024-04-17 10:47:28 +0200

    Logout button w/ fn added to 'Appbar'

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,14 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "./Button";
 
 interface AppbarInterface{
     initial: string;
 }
 
 export function Appbar(props: AppbarInterface){
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        try{
+            localStorage.removeItem('token');
+            navigate('/signin');
+        }catch(err){
+            console.error('An error occurred', err);      
+        }
+    };
+
     return (
     <div className="border-solid border-2 border-gray-300 shadow h-14 flex justify-between">
         <div className="flex flex-col justify-center h-full ml-4">
-            {/* PayPer */}
             <Link to="/dashboard">PayPer</Link>
         </div>
         <div className="flex">
@@ -19,6 +30,9 @@ export function Appbar(props: AppbarInterface){
                 <div className="flex flex-col justify-center h-full text-xl text-white">
                     {props.initial}
                 </div>
+            </div>            
+            <div className="flex flex-col justify-center h-full mr-4">
+                <Button label="Logout" className="mb-4" onClick={handleLogout} />
             </div>
         </div>
     </div>
