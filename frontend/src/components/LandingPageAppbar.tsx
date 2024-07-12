f155326965d2b8ebf1fce09a0d5fafe7166d4ebe
Tabish1511 <khaqantabish@gmail.com>
2024-07-12 15:45:56 +0200

    Local - Hero done, do the banner component to be applied multiple times

--- frontend/src/components/LandingPageAppbar.tsx
+++ frontend/src/components/LandingPageAppbar.tsx
@@ -4,7 +4,7 @@ export function LandingPageAppbar(){
     return (
         <div className="bg-white rounded-lg h-16 w-3/4 flex flex-col justify-center">
             <div className="flex justify-between px-4">
-                <div className="text-3xl font-black flex flex-col justify-center text-center">
+                <div className="text-4xl font-black flex flex-col justify-center text-center">
                     P
                 </div>
                 <div className="flex">
--- frontend/src/pages/landingPage.tsx
+++ frontend/src/pages/landingPage.tsx
@@ -1,14 +1,18 @@
 import { HeroComponent } from "../components/HeroComponent";
 import { LandingPageAppbar } from "../components/LandingPageAppbar";
+import dashboardLogo from "../assets/Screenshot from 2024-07-12 15-22-06.png"
 
 export function LandingPage(){
     return (
-        <div className="bg-cream-main h-screen p-6">
+        <div className="bg-cream-main h-auto p-6">
             <div className="w-full flex justify-center">
                 <LandingPageAppbar />
             </div>
-            <div className="flex justify-center my-20">
+            <div className="flex justify-center mt-40">
                 <HeroComponent />
+            </div>
+            <div className="flex justify-center">
+                <img src={dashboardLogo} className="w-3/4 h-auto"></img>
             </div>
         </div>
     )
