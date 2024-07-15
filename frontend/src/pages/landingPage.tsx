0096c9f9cb4189d0915d20b1f4531de660114007
Tabish1511 <khaqantabish@gmail.com>
2024-07-15 16:10:20 +0200

    local - Reusable BannerComponent.tsx done, populate the landing page/create footer

--- frontend/src/pages/landingPage.tsx
+++ frontend/src/pages/landingPage.tsx
@@ -1,6 +1,7 @@
 import { HeroComponent } from "../components/HeroComponent";
 import { LandingPageAppbar } from "../components/LandingPageAppbar";
 import dashboardLogo from "../assets/Screenshot from 2024-07-12 15-22-06.png"
+import cloudLogo from '../assets/undraw_cloud_sync_re_02p1.svg';
 import { BannerComponent } from "../components/BannerComponent";
 
 export function LandingPage(){
@@ -15,7 +16,9 @@ export function LandingPage(){
             <div className="flex justify-center">
                 <img src={dashboardLogo} className="w-3/4 h-auto"></img>
             </div>
-            <BannerComponent />
+            <BannerComponent 
+                text="Access your dashboard on the go as the platform stands on serverless cloud architecture"
+                icon={cloudLogo}/>
         </div>
     )
 }
