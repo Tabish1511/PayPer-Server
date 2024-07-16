c74e5b457d45ab7cd7400268452a613613261154
Tabish1511 <khaqantabish@gmail.com>
2024-07-16 14:32:49 +0200

    local - 90% landingpage done, left with footer.tsx

--- frontend/src/components/BannerComponentAlt.tsx
+++ frontend/src/components/BannerComponentAlt.tsx
@@ -1,11 +1,11 @@
-interface BannerComponentProps {
+interface BannerComponentPropsAlt {
     text: string;
     icon: string;
 }
 
-export function BannerComponent(props: BannerComponentProps){
+export function BannerComponentAlt(props: BannerComponentPropsAlt){
     return (
-        <div className="bg-white rounded-lg h-96 w-2/4 p-16 flex flex-col justify-center">
+        <div className="bg-cream-main rounded-lg h-96 w-2/4 p-16 flex flex-col justify-center">
             <div className="flex justify-around h-72">
                 <div className="text-xl w-5/12 flex flex-col justify-center text-left">
                     {props.text}
--- frontend/src/pages/landingPage.tsx
+++ frontend/src/pages/landingPage.tsx
@@ -2,11 +2,16 @@ import { HeroComponent } from "../components/HeroComponent";
 import { LandingPageAppbar } from "../components/LandingPageAppbar";
 import dashboardLogo from "../assets/Screenshot from 2024-07-12 15-22-06.png"
 import cloudLogo from '../assets/undraw_cloud_sync_re_02p1.svg';
+import prismaLogo from '../assets/prisma-3.svg';
+import reactTailwindLogo from '../assets/reactAndTailwind.png';
+import mailSentLogo from '../assets/mail_sent.jpeg';
+import mailSentSvg from '../assets/undraw_mail_sent_re_0ofv.svg';
 import { BannerComponent } from "../components/BannerComponent";
+import { BannerComponentAlt } from "../components/BannerComponentAlt";
 
 export function LandingPage(){
     return (
-        <div className="bg-cream-main h-auto p-6">
+        <div className="bg-cream-main h-auto pt-6">
             <div className="w-full flex justify-center">
                 <LandingPageAppbar />
             </div>
@@ -16,10 +21,25 @@ export function LandingPage(){
             <div className="flex justify-center">
                 <img src={dashboardLogo} className="w-3/4 h-auto"></img>
             </div>
-            <div className="flex justify-center mt-40">
+            <div className="py-40 flex justify-center">
                 <BannerComponent 
                     text="Access your dashboard on the go as the platform stands on serverless cloud architecture"
                     icon={cloudLogo}/>
+            </div>
+            <div className="bg-black py-40 flex justify-center">
+                <BannerComponentAlt 
+                    text="As your business’ sales numbers grow PayPer grows with you thanks to PrismaDB’s scalability capabilities"
+                    icon={prismaLogo}/>
+            </div>
+            <div className="py-40 flex justify-center">
+                <BannerComponent 
+                    text="Never a hiccup in accessing your dashboard as Tailwindcss and Reactjs make the UI/UX experience a breeze"
+                    icon={reactTailwindLogo}/>
+            </div>
+            <div className="bg-black py-40 flex justify-center">
+                <BannerComponentAlt 
+                    text="Get email notifications on every step of the way in your journey to grow a successful business"
+                    icon={mailSentSvg}/>
             </div>
         </div>
     )
